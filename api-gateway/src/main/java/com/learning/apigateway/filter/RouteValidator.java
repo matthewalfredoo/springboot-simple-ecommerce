bd6500216e40af0dc81c1d914559f5fd169fc795
Matthew Alfredo <matthewwalfredoo@gmail.com>
2023-09-06 22:59:17 +0700

    ag - change RouteValidator to check request method

--- api-gateway/src/main/java/com/learning/apigateway/filter/RouteValidator.java
+++ api-gateway/src/main/java/com/learning/apigateway/filter/RouteValidator.java
@@ -1,22 +1,35 @@
 package com.learning.apigateway.filter;
 
+import org.springframework.http.HttpMethod;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.function.Predicate;
 
 @Component
 public class RouteValidator {
 
-    public static final List<String> openApiEndpoints = List.of(
-            "/api/v1/auth-service/login",
-            "/api/v1/auth-service/register"
+    private static final Map<String, Set<HttpMethod>> openApiEndpoints = Map.of(
+            // endpoint, methods
+
+            // auth-service
+            "/api/v1/auth/login", Set.of(HttpMethod.POST),
+            "/api/v1/auth/register", Set.of(HttpMethod.POST),
+
+            // product-service
+            "/api/v1/products", Set.of(HttpMethod.GET) // this also includes endpoint /api/v1/products/{id} with GET method
     );
 
     public Predicate<ServerHttpRequest> isSecured =
             serverHttpRequest -> openApiEndpoints
+                    .keySet()
                     .stream()
-                    .noneMatch(uri -> serverHttpRequest.getURI().getPath().contains(uri));
+                    .noneMatch(uri ->
+                            serverHttpRequest.getURI().getPath().contains(uri) &&
+                                    openApiEndpoints.get(uri).contains(serverHttpRequest.getMethod())
+                    );
 
 }
