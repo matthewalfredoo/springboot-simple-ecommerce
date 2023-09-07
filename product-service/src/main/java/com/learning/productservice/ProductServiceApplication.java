b06e977656ba7fa4462287f611f35cc2fb9a9705
Matthew Alfredo <matthewwalfredoo@gmail.com>
2023-09-07 13:58:22 +0700

    ps - missing commit when migrating to MongoDB

--- product-service/src/main/java/com/learning/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/learning/productservice/ProductServiceApplication.java
@@ -2,9 +2,12 @@ package com.learning.productservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories
 public class ProductServiceApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductServiceApplication.class, args);
