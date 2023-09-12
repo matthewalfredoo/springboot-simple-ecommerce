e1fff929ee6badd2cd8750e535af6b537eed904d
Matthew Alfredo <matthewwalfredoo@gmail.com>
2023-09-12 21:27:05 +0700

    ps - handle fallback and circuitbreaker openfeign

---
+++ product-service/src/main/java/com/learning/productservice/config/FeignConfig.java
@@ -0,0 +1,31 @@
+package com.learning.productservice.config;
+
+import com.learning.productservice.proxy.CachingServiceProxy;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.Duration;
+
+@Configuration
+public class FeignConfig {
+
+    @Bean
+    public Customizer<Resilience4JCircuitBreakerFactory> circuitBreakerFactoryCustomizer() {
+        CircuitBreakerConfig circuitBreakerConfig = CircuitBreakerConfig.custom()
+                .slidingWindowType(CircuitBreakerConfig.SlidingWindowType.COUNT_BASED)
+                .slidingWindowSize(5)
+                .minimumNumberOfCalls(5)
+                .failureRateThreshold(20.0f)
+                .waitDurationInOpenState(Duration.ofSeconds(5))
+                .permittedNumberOfCallsInHalfOpenState(5)
+                .build();
+
+        return resilience4JCircuitBreakerFactory -> resilience4JCircuitBreakerFactory.configure(builder ->
+                builder.circuitBreakerConfig(circuitBreakerConfig), CachingServiceProxy.FEIGN_CLIENT_NAME
+        );
+    }
+
+}
--- product-service/src/main/java/com/learning/productservice/proxy/CachingServiceProxy.java
+++ product-service/src/main/java/com/learning/productservice/proxy/CachingServiceProxy.java
@@ -4,8 +4,10 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name="caching-service")
+@FeignClient(name="caching-service", fallback = CachingServiceProxyFallback.class)
 public interface CachingServiceProxy {
+
+    String FEIGN_CLIENT_NAME = "caching-service";
 
     @GetMapping("/api/v1/cache/products/response")
     public String getApiResponseJson(
